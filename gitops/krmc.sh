d56ae3ad3e25793101943be5ae53dc3c1b316d04
Mats Andresen <mats.andresen@gmx.de>
2024-04-13 00:05:33 +0200

    feat(gitops/krmc): support passing flags to kustomize with `--kustomize-flags` (#22)
    krmc: support passing flags to kustomize with `--kustomize-flags`, therefore `--kustomize-load-restrictor` has been removed.

--- gitops/krmc.sh
+++ gitops/krmc.sh
@@ -41,10 +41,6 @@ find_kustomization_files() {
 
 # Build
 build() {
-  local kustomize_flags=(
-    "--load-restrictor=$kustomize_load_restrictor"
-  )
-
   find_kustomization_files "$1" | while IFS= read -r -d $'\0' file;
     do
       working_dir=$(dirname "$file")
@@ -53,7 +49,7 @@ build() {
       purple "$working_dir"
 
       set +e
-      output=$(kustomize build "$working_dir" "${kustomize_flags[@]}" 2>&1)
+      output=$(kustomize build "$working_dir" "$kustomize_flags" 2>&1)
       outcome=${PIPESTATUS[0]}
       set -e
 
@@ -152,7 +148,7 @@ user_manual() {
   echo
   echo "Flags:"
   printf "  --kubernetes-version\t\tThe kubernetes version to validate against. (default: 1.27.4)\n"
-  printf "  --kustomize-load-restrictor\tThe kustomize load restrictor to use. (default: LoadRestrictionsNone)\n"
+  printf "  --kustomize-flags\t\tThe kustomize flags to use. (default: --load-restrictor=LoadRestrictionsNone)\n"
   printf "  --trivy-severity\t\tThe trivy severity to fail on. (default: HIGH,CRITICAL,MEDIUM)\n"
   printf "  --trivy-ignorefile\t\tThe trivy ignorefile to use. (default: .trivyignore)\n"
   printf "  --help\t\t\tHelp for krmc usage.\n"
@@ -162,13 +158,13 @@ user_manual() {
 print_settings() {
   purple "KRMC (Kubernetes Resource Model Checker):"
   echo
-  printf "command:\t\t\t%s\n" "$command"
-  printf "working dir:\t\t\t%s\n" "${working_dir[@]}"
+  printf "command:\t\t%s\n" "$command"
+  printf "working dir:\t\t%s\n" "${working_dir[@]}"
   echo
-  printf "kubernetes-version:\t\t%s\n" "$kubernetes_version"
-  printf "kustomize-load-restrictor:\t%s\n" "$kustomize_load_restrictor"
-  printf "trivy-severity:\t\t\t%s\n" "$trivy_severity"
-  printf "trivy-ignorefile:\t\t%s\n" "$trivy_ignorefile"
+  printf "kubernetes-version:\t%s\n" "$kubernetes_version"
+  printf "kustomize-flags:\t%s\n" "$kustomize_flags"
+  printf "trivy-severity:\t\t%s\n" "$trivy_severity"
+  printf "trivy-ignorefile:\t%s\n" "$trivy_ignorefile"
   echo
 }
 
@@ -178,7 +174,7 @@ main() {
   # options
   export command="help"
   export working_dir=()
-  export kustomize_load_restrictor="LoadRestrictionsNone"
+  export kustomize_flags="--load-restrictor=LoadRestrictionsNone"
   export kubernetes_version="1.27.4"
   export trivy_severity="HIGH,CRITICAL,MEDIUM"
   export trivy_ignorefile=".trivyignore"
@@ -208,8 +204,8 @@ main() {
         kubernetes_version="${arg#*=}"
         shift # past argument=value
         ;;
-      --kustomize-load-restrictor=*)
-        kustomize_load_restrictor="${arg#*=}"
+      --kustomize-flags=*)
+        kustomize_flags="${arg#*=}"
         shift # past argument=value
         ;;
       --trivy-severity=*)
